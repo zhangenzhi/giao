ffcbe71a4e90846d14241a5f54265b1aff58e6cd
张恩智 <zhangenzhi657@gmail.com>
2023-02-01 08:34:37 +0900

    feat: update UNet to CUnet.

--- unet.py
+++ unet.py
@@ -61,3 +61,95 @@ class UNet:
     
     def __call__(self, inputs):
         return self.model(inputs)
+    
+def downsample(filters, size, apply_batchnorm=True):
+        initializer = tf.random_normal_initializer(0., 0.02)
+
+        result = tf.keras.Sequential()
+        result.add(
+            tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                                    kernel_initializer=initializer, use_bias=False))
+
+        if apply_batchnorm:
+            result.add(tf.keras.layers.BatchNormalization())
+
+        result.add(tf.keras.layers.LeakyReLU())
+
+        return result
+    
+def upsample(filters, size, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                        padding='same',
+                                        kernel_initializer=initializer,
+                                        use_bias=False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+
+    result.add(tf.keras.layers.ReLU())
+
+    return result
+    
+class CUNet:
+    
+    def __init__(self, input_shape=[32,32,3]) -> None:
+        self.input_shape = input_shape
+        self.OUTPUT_CHANNELS = 3
+        self.model = self.Generator()
+    
+    def Generator(self):
+        inputs = tf.keras.layers.Input(shape=self.input_shape)
+
+        down_stack = [
+            downsample(32, 4, apply_batchnorm=False),  # (batch_size, 16, 16, 64)
+            downsample(64, 4),  # (batch_size, 8, 8, 128)
+            downsample(128, 4),  # (batch_size, 4, 4, 256)
+            downsample(256, 4),  # (batch_size, 2, 2, 512)
+            downsample(512, 4),  # (batch_size, 1, 1, 512)
+        ]
+
+        up_stack = [
+            upsample(512, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
+            upsample(256, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
+            upsample(128, 4),  # (batch_size, 8, 8, 512)
+            upsample(64, 4),  # (batch_size, 16, 16, 256)
+            upsample(32, 4),  # (batch_size, 32, 32, 128)
+        ]
+
+        initializer = tf.random_normal_initializer(0., 0.02)
+        last = tf.keras.layers.Conv2DTranspose(self.OUTPUT_CHANNELS, 4,
+                                                strides=2,
+                                                padding='same',
+                                                kernel_initializer=initializer,
+                                                activation='tanh')  # (batch_size, 32, 32, 3)
+
+        x = inputs
+
+        # Downsampling through the model
+        skips = []
+        for down in down_stack:
+            x = down(x)
+            skips.append(x)
+
+        skips = reversed(skips[:-1])
+
+        # Upsampling and establishing the skip connections
+        for up, skip in zip(up_stack, skips):
+            x = up(x)
+            x = tf.keras.layers.Concatenate()([x, skip])
+
+        x = last(x)
+
+        return tf.keras.Model(inputs=inputs, outputs=x)
+    
+    def plot_model(self):
+        tf.keras.utils.plot_model(self.model, show_shapes=True)
+    
+    def __call__(self, inputs):
+        return self.model(inputs)
